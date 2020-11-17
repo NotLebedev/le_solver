65d1a5764381625b5dc4b76cf83caae4c05a9332
artemiy <artem-itf@yandex.ru>
2020-11-17 20:47:34 +0300

    *Changed matrix print format

--- matrix.c
+++ matrix.c
@@ -47,11 +47,15 @@ mul_sub_row(Matrix *matrix, size_t s, size_t d, data_t c)
 void
 print_matrix(FILE *file, Matrix *matrix)
 {
+    fputs("{", file);
     for (size_t i = 0; i < matrix->row; i++) {
+        fputs("{", file);
         for (size_t j = 0; j < matrix->col; j++) {
-            fprintf(file, "%10.2" PR_DATA_T " ", matrix->values[j + matrix->col * i]);
+            fprintf(file, "%.2" PR_DATA_T "%s", matrix->values[j + matrix->col * i],
+                    j != matrix-> col - 1 ? ", " : "");
         }
-        fputs("\n", file);
+        fputs(i != matrix->row - 1 ? "}," : "}", file);
     }
+    fputs("}\n", file);
 }
 
