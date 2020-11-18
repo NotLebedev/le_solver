b3c7a42feac65a83cef6cf01237e1ffdb5e3104a
artemiy <artem-itf@yandex.ru>
2020-11-18 15:51:59 +0300

    +Docs

--- debug.h
+++ debug.h
@@ -1,10 +1,12 @@
 #ifndef LE_SOLVER_DEBUG_H
 #define LE_SOLVER_DEBUG_H
 
-#define ASSERT_MATRIX_RANGES
-#define ASSERT_INCORRECT_ARGUMENTS
+#define ASSERT_MATRIX_RANGES // Выполнять проверку индексации матрицы в функциях, объявленных в matrix.h
+#define ASSERT_INCORRECT_ARGUMENTS // Выполнять проверку коректности аргументо в функциях, объявленных в algs.h
 
-//#define PRINT_MATRIX_HUMAN_READABLE
-#define PRINT_MATRIX_ELIMINATION_ITERATIONS
+#define PRINT_MATRIX_HUMAN_READABLE // Выводить матрицы функцией print_matrix в виде квадратных матриц или в виде
+// списков списков {{1, 2}, {3, 4}} как в многих математических программах
+#define PRINT_MATRIX_ELIMINATION_ITERATIONS // Выводить этапы прямого хода метода Гаусса в функциях, объявленных
+// в algs.h
 
 #endif //LE_SOLVER_DEBUG_H
