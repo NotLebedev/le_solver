06f29a85f91c6dedffe49925ebef8e8918ab38e0
NotLebedev <artem-itf@yandex.ru>
2020-11-20 13:22:53 +0300

    -Removed redundant doxygen docs

--- matrix.h
+++ matrix.h
@@ -20,14 +20,9 @@ Matrix *copy_matrix(Matrix *matrix);
 
 data_t get_element(Matrix *matrix, size_t row, size_t col);
 void set_element(Matrix *matrix, size_t row, size_t col, data_t val);
-/**
- * Прибавить к строке d матрицы a строку s, умноженную на c
- * @param matrix матрица для преобразования
- */
+
 void mul_sub_row(Matrix *matrix, size_t s, size_t d, data_t c);
-
 void mul_row(Matrix *matrix, size_t row, data_t c);
-
 void swap_row(Matrix *matrix, size_t s, size_t d);
 
 void print_matrix(FILE *file, Matrix *matrix);
