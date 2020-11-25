1f735e2468ec532be69fe44c2d00dfeb78c8825a
NotLebedev <artem-itf@yandex.ru>
2020-11-25 20:13:28 +0300

    +Arbitrary print mode selection

--- matrix.c
+++ matrix.c
@@ -42,26 +42,28 @@ void mul_sub_row(Matrix *matrix, size_t s, size_t d, data_t c) {
     }
 }
 
+int mode = 0;
+
 void print_matrix(FILE *file, Matrix *matrix) {
-#ifdef PRINT_MATRIX_HUMAN_READABLE
-    for (size_t i = 0; i < matrix->row; i++) {
-        for (size_t j = 0; j < matrix->col; j++) {
-            fprintf(file, "%10.5" PR_DATA_T " ", matrix->values[j + matrix->col * i]);
+    if (mode == 0) {
+        for (size_t i = 0; i < matrix->row; i++) {
+            for (size_t j = 0; j < matrix->col; j++) {
+                fprintf(file, "%10.5" PR_DATA_T " ", matrix->values[j + matrix->col * i]);
+            }
+            fputs("\n", file);
         }
-        fputs("\n", file);
-    }
-#else
-    fputs("{", file);
-    for (size_t i = 0; i < matrix->row; i++) {
+    } else {
         fputs("{", file);
-        for (size_t j = 0; j < matrix->col; j++) {
-            fprintf(file, "%" PR_DATA_T "%s", matrix->values[j + matrix->col * i],
-                    j != matrix-> col - 1 ? ", " : "");
+        for (size_t i = 0; i < matrix->row; i++) {
+            fputs("{", file);
+            for (size_t j = 0; j < matrix->col; j++) {
+                fprintf(file, "%20.20" PR_DATA_T "%s", matrix->values[j + matrix->col * i],
+                        j != matrix->col - 1 ? ", " : "");
+            }
+            fputs(i != matrix->row - 1 ? "}," : "}", file);
         }
-        fputs(i != matrix->row - 1 ? "}," : "}", file);
+        fputs("}\n", file);
     }
-    fputs("}\n", file);
-#endif
 }
 
 void swap_row(Matrix *matrix, size_t s, size_t d) {
